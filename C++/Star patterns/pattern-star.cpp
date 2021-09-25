5f7f0d1050b250441eefa6b06056d34b3ea0f22a
Zakariya khan <73132031+Ardent10@users.noreply.github.com>
2021-09-25 12:20:03 +0530

    Star pattern code formatting

--- C++/Star patterns/pattern-star.cpp
+++ C++/Star patterns/pattern-star.cpp
@@ -3,11 +3,17 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
+    int n;
+    cout<<"Enter the number of rows: ;
+    cin>>n;
+    
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j <= i; j++)
-            cout << "*";
-        cout << endl;
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout<<endl;
     }
+    
+return 0;
 }
